6c77158c1c4644e5a71ca1cd4fdb858819326c45
Sasa <sasa.seles@valosstudio.com>
2023-02-12 16:42:46 +0900

    RC 1 update

--- Strategy Tutorial 1/Characters/Cameras/MainCamera.cs
+++ Strategy Tutorial 1/Characters/Cameras/MainCamera.cs
@@ -6,74 +6,74 @@ namespace StrategyTutorial1.Characters.Cameras;
 
 public partial class MainCamera : Camera3D
 {
-    [Export()] public float RayLength { get; set; } = 100f;
+	[Export()] public float RayLength { get; set; } = 100f;
 
-    private uint _collisionMask;
-    private PackedScene _build;
-    private Array _excluded;
-    private World3D _world;
-    private Window _root;
+	private uint _collisionMask;
+	private PackedScene _build;
+	private Array _excluded;
+	private World3D _world;
+	private Window _root;
 
-    public override void _Ready()
-    {
-        GameplaySignals gameplaySignals = GetNode<GameplaySignals>("/root/GameplaySignals");
+	public override void _Ready()
+	{
+		GameplaySignals gameplaySignals = GetNode<GameplaySignals>("/root/GameplaySignals");
 
-        gameplaySignals.PlaceObject += GameplaySignalsOnPlaceObject;
+		gameplaySignals.PlaceObject += GameplaySignalsOnPlaceObject;
 
-        _excluded = new Array { this };
+		_excluded = new Array { this };
 
-        _world = GetWorld3D();
-        
-        _root = GetTree().Root;
+		_world = GetWorld3D();
+		
+		_root = GetTree().Root;
 
-        SetPhysicsProcess(false);
-    }
+		SetPhysicsProcess(false);
+	}
 
-    private void GameplaySignalsOnPlaceObject(PackedScene buildable, uint mask)
-    {
-        SetPhysicsProcess(buildable != null);
+	private void GameplaySignalsOnPlaceObject(PackedScene buildable, uint mask)
+	{
+		SetPhysicsProcess(buildable != null);
 
-        _build = buildable;
+		_build = buildable;
 
-        _collisionMask = mask;
-    }
+		_collisionMask = mask;
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (Input.IsActionPressed("Cancel"))
-        {
-            GameplaySignalsOnPlaceObject(null, 0);
-            
-            return;
-        }
+	public override void _PhysicsProcess(double delta)
+	{
+		if (Input.IsActionPressed("Cancel"))
+		{
+			GameplaySignalsOnPlaceObject(null, 0);
+			
+			return;
+		}
 
-        Dictionary collisions = DetectCollisions();
+		Dictionary collisions = DetectCollisions();
 
-        if (collisions.Count > 0)
-        {
-            if (Input.IsActionPressed("Action"))
-            {
-                Node3D scene = _build.Instantiate<Node3D>();
+		if (collisions.Count > 0)
+		{
+			if (Input.IsActionPressed("Action"))
+			{
+				Node3D scene = _build.Instantiate<Node3D>();
 
-                _root.AddChild(scene);
+				_root.AddChild(scene);
 
-                scene.GlobalPosition = (Vector3)collisions["position"];
-            }
-        }
-    }
+				scene.GlobalPosition = (Vector3)collisions["position"];
+			}
+		}
+	}
 
-    private Dictionary DetectCollisions()
-    {
-        PhysicsDirectSpaceState3D spaceState = _world.DirectSpaceState;
+	private Dictionary DetectCollisions()
+	{
+		PhysicsDirectSpaceState3D spaceState = _world.DirectSpaceState;
 
-        Vector2 mouse = GetViewport().GetMousePosition();
+		Vector2 mouse = GetViewport().GetMousePosition();
 
-        Vector3 from = ProjectRayOrigin(mouse);
+		Vector3 from = ProjectRayOrigin(mouse);
 
-        Vector3 to = from + ProjectRayNormal(mouse) * RayLength;
+		Vector3 to = from + ProjectRayNormal(mouse) * RayLength;
 
-        PhysicsRayQueryParameters3D parameters = PhysicsRayQueryParameters3D.Create(from, to, _collisionMask, null);
+		PhysicsRayQueryParameters3D parameters = PhysicsRayQueryParameters3D.Create(from, to, _collisionMask, null);
 
-        return spaceState.IntersectRay(parameters);
-    }
+		return spaceState.IntersectRay(parameters);
+	}
 }
